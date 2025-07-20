a83120ece4acb43ac34ee6082bd4d68355288ad2
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-20 14:57:30 +0500

    create login and register serializers

---
+++ user/serializers/register.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from ..models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'phone_number', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser.objects.create_user(password=password, **validated_data)
+        return user
