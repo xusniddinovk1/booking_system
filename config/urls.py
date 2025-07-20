929d1aace651cdea2216c7ac191a898257d3af31
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-20 15:28:45 +0500

    update urls

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,15 @@
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/', include('hotel.urls')),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
