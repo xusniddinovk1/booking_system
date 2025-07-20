01c563b3d8440b6220b744ac9c0ceb2e5ce6d468
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-20 15:33:51 +0500

    update settings

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openaiAutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
