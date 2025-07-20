ea9c9dad1ee7982bb3652310db602fe267b1de40
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-20 15:38:02 +0500

    update urls

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,8 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/', include('hotel.urls')),
+    # path('api/', include('hotel.urls')),
+    path('api/users/', include('user.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
