20c1f2ae65628b4cc979deeb13c6f38b490a2b68
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-27 08:56:57 +0500

    add roombooking urls

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from hotel.views.hotel import HotelViewSet, RoomViewSet
+from hotel.views.booking import RoomBookingViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'hotels', HotelViewSet)
 router.register(r'rooms', RoomViewSet)
+router.register(r'room-bookings', RoomBookingViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
