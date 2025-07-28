cafd98f6b7d7ccc41a9bc53e3c6c74553bff5e6f
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-28 16:11:07 +0500

    initial commit

--- booking/urls.py
+++ booking/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from ..views import HotelVIewSet, RoomViewSet, RoomBookingViewSet, TransportViewSet, TransportBookingViewSet
+from .views import HotelViewSet, RoomViewSet, RoomBookingViewSet, TransportViewSet, TransportBookingViewSet
 
 router = DefaultRouter()
-router.register(r'hotels', HotelVIewSet)
+router.register(r'hotels', HotelViewSet)
 router.register(r'rooms', RoomViewSet)
 router.register(r'room-booking', RoomBookingViewSet)
 router.register(r'transports', TransportViewSet)
