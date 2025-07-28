c362ff608d045768973c671487195071418cc3c8
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-28 09:07:31 +0500

    add Hotel,Room model

---
+++ booking/models/hotel.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    rating = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+ROOM_TYPES = (
+    ('SINGLE', 'Single'),
+    ('DOUBLE', 'Double'),
+    ('SUITE', 'Suite')
+)
+
+
+class Room(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    room_number = models.PositiveIntegerField()
+    room_type = models.CharField(max_length=6, choices=ROOM_TYPES)
+    price = models.DecimalField(max_digits=10, decimal_places=3)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.room_number
